9b6bc80ef81bd6e2231b1ba442efaa03980a594e
Matt Renaud <mrenaud92@gmail.com>
2013-11-05 14:35:05 -0500

    Added greeting and changed prompt in query program.

--- query_secondary_index.cxx
+++ query_secondary_index.cxx
@@ -21,16 +21,25 @@ int main(int argc, char* argv[])
 	key_table.load(std::string(argv[1]));
 	key_table.set_pk_filename(std::string(argv[2]));
 
+	// Display greeting.
+	std::cout << "Welcome to secondary file query processor.\n"
+	          << "  Key Table: " << argv[1] << "\n"
+	          << "  Key File:  " << argv[2] << "\n\n";
+
 	// Read a secondary key and print all records with that as the secondary key.
 	std::string key;
-	std::cout << "Enter a secondary key: ";
+	std::cout << "Query> ";
 
 	while (std::cin >> key)
 	{
 		secondary_key_iterator<car_owner> i = key_table.find<car_owner>(key);
-		for (; i != secondary_key_iterator<>(); ++i)
-			std::cout << *i << std::endl;
+		secondary_key_iterator<car_owner> end;
+		if (i == end)
+			std::cout << "No entries with secondary key " << key << ".\n";
+		else
+			for (; i != secondary_key_iterator<>(); ++i)
+				std::cout << "  --------\n" << *i << std::endl;
 
-		std::cout << "Enter a secondary key: ";
+		std::cout << "\nQuery> ";
 	}
 }
